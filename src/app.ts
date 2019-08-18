d08ea6c79d32e9ea562ae3e8e22258eb5f25967d
Igor Soroka <stupbird@gmail.com>
2019-08-18 10:50:12 +0300

    return names of sorted packages

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import PackagesFileParser from "./packagesFileParser";
 
 export const app: express.Application = express();
 
 const mainRoute: string = "/api/packages";
 
+const packagesFileParser: PackagesFileParser = new PackagesFileParser("../files/status.real");
+const fileContent: string = packagesFileParser.readFile();
+
 app.get(`${mainRoute}/`, (req, res) => {
-  res.status(200).send("List of all packages");
+  const moduleNames: string[] = packagesFileParser.getAllModuleNames(fileContent);
+  res.status(200).send({
+    modules: moduleNames
+  });
 });
 
 app.get(`${mainRoute}/:package-name`, (req, res) => {
