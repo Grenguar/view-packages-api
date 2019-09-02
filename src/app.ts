731a304df6b311d72542020bbad449cbf54a1cf4
Igor Soroka <stupbird@gmail.com>
2019-09-02 21:18:59 +0300

    better catching and handling errors

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import PackagesServiceV1 from "./service/packagesServiceV1";
 import { IPackageInfo, IPackageInfoHAL } from "./domain/packageInfo";
 import PackagesServiceV2 from "./service/packagesServiceV2";
 import HALlink from "./domain/halLink";
+import IError from "./domain/error";
 
 export const app: express.Application = express();
 
@@ -12,10 +13,17 @@ const mainRouteV2: string = "/api/v2/packages";
 const packagesFilePath: string = "../../files/status.real";
 const packagesService: PackagesServiceV1 = new PackagesServiceV1(packagesFilePath);
 const packagesServiceV2: PackagesServiceV2 = new PackagesServiceV2(packagesFilePath);
+const successCode: number = 200;
+const clientErrorCode: number = 400;
+const packageNotFoundError: IError = {
+  type: "Package Not Found Error",
+  status: 400,
+  message: "Check the name of the package from packages list by calling main endpoint",
+};
 
 app.get(`${mainRoute}/`, (req, res) => {
   const moduleNames: string[] = packagesService.getNames();
-  res.status(200).send({
+  res.status(successCode).send({
     packages: moduleNames,
   });
 });
@@ -24,9 +32,9 @@ app.get(`${mainRoute}/:package`, (req, res) => {
   try {
     const params: any = req.params;
     const packageInfo: IPackageInfo | null = packagesService.getPackageInfo(params.package);
-    res.status(200).send(packageInfo);
+    res.status(successCode).send(packageInfo);
   } catch (e) {
-    res.status(400).send("Error occured. Check the name of the package from packages list");
+    res.status(clientErrorCode).send(packageNotFoundError);
   }
 });
 
@@ -35,7 +43,7 @@ app.get(`${mainRouteV2}/`, (req, res) => {
   const packageNamesHAL: HALlink[] = packagesServiceV2.getAllPackagesNamesWithHAL(hostPath);
   res
     .set("Content-Type", "application/hal+json")
-    .status(200)
+    .status(successCode)
     .send({
       packages: packageNamesHAL,
     });
@@ -48,10 +56,10 @@ app.get(`${mainRouteV2}/:package`, (req, res) => {
     const packageInfo: IPackageInfoHAL | null = packagesServiceV2.getPackageInfoWithHAL(params.package, hostPath);
     res
       .set("Content-Type", "application/hal+json")
-      .status(200)
+      .status(successCode)
       .send(packageInfo);
   } catch (e) {
-    res.status(400).send("Error occured. Check the name of the package or the file");
+    res.status(clientErrorCode).send(packageNotFoundError);
   }
 });
 
---
+++ src/domain/error.d.ts
@@ -0,0 +1,5 @@
+export default interface IError {
+  type: string;
+  status: number;
+  message: string;
+}
